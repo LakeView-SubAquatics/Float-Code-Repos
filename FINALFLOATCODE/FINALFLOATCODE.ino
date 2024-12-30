8094f51feef248f7fe414c94d5e7eb75fc06e19f
Professor E <Megakid11898@gmail.com>
2024-12-30 13:54:50 -0600

    Update FINALFLOATCODE.ino
    Major Updates To main loop of code

--- FINALFLOATCODE/FINALFLOATCODE.ino
+++ FINALFLOATCODE/FINALFLOATCODE.ino
@@ -2,21 +2,8 @@
   Author(s): Tyerone Chen, Danny Henningfield, Adam Palma, 
 
     Innit Create: 6/30/2024
-      Last update: 12/29/2024
+      Last update: 12/30/2024
 */
-
-// ** NEEDED CHANGES REMINDER ** //
-// Remove a bunch of the serial prints or whatever so we can burn the code onto the arduino. - DONE
-// Set any use of the pinmodesin the code as referances as to help with readability of the code.
-// Remake the code within the MAIN LOOP Program to be in functions which'll be called in
-// Remake the motor process code to bealigned with the new competition requirements.
-  // Try to setup enum data types and terenary operator for simpler and much more enhanced code?
-  // Make sure to include "digitalWrite(LED_BUILTIN, LOW);" in any code which changes motor movement
-    // THis is to ensure that the switches have a resistor for the voltage that is passed through
-// Add some necessary checks and failsafes for the radio transmission code.
-// Genrally clean up whatever miss haps and naming issues there are in the code.
-// Remove or comment out any methods refering to psi change.
-// Change the recieved data info into a status codes
 
 // Arduino Float Code Remake
 
@@ -24,17 +11,14 @@
 // aneurism reading the old code （´∇｀''）
 // Adam note: me too, i am now brain damaged .-.
 
-
 // Included Library
 #include <SPI.h>
 #include <RH_RF95.h> // Used for the radio and specific adafruit board used
 #include <ezButton.h> // Used for the much better button/switch detection
 #include <List.hpp> // Used for the simpler functionality to make lists, instead of having to manually redefine arrays.
 
-
-
-// Init definition and crap
-//Radio crap
+// Innitial Definitions
+  // Radio Communications Setup
 #if defined(ADAFRUIT_FEATHER_M0) || defined(ADAFRUIT_FEATHER_M0_EXPRESS) || defined(ARDUINO_SAMD_FEATHER_M0)
 #define RFM95_CS    8
 #define RFM95_INT   3
@@ -42,19 +26,19 @@
 #endif
 
 #define RF95_FREQ 915.0
-
 RH_RF95 rf95(RFM95_CS, RFM95_INT);
 
 #pragma region Variable_Definement
 
-// list defin area - Type: float, int
+// List Definition
+  // psiList - float
+  // depthList - float
+  // timeList - int
 List<float> psiList;
 List<float> depthList;
 List<int> timeList;
 
-// Data recived innit
-// essentialy this is where the cariable of the recieved data, from the pi, will be held
-// should always be a string, which is formated as a list of chars
+// Recievement Data Innitializer
 char received_data[RH_RF95_MAX_MESSAGE_LEN];
 
 // PSI Calculation Variables
@@ -62,7 +46,7 @@ float psi_half_sec = 0;
 float psi_full_sec = 0;
 float psi_calc = 0;
 
-// Arduino & Motor Port Connection Variables
+// Arduino & Motor Port Connection Variables Definition
 int outA = 5;
 int diag_port_A = 6;
 
@@ -73,8 +57,8 @@ int pwm_port = 9;
 
 const DUTY_CYCLE = 255;
 
-ezButton switch_top(12);   // Top Siwtch Connected to pin 12
-ezButton switch_bottom(A3);// Bottom switch connected to pin A3
+ezButton switch_top(12);   // Top Swtch Connected to pin 12
+ezButton switch_bottom(A3);// Bottom Switch connected to pin A3
 
 // Enum for Float States
   // SURFACED - 
@@ -86,7 +70,7 @@ enum Float_State {
   SUBMURSED,
   MOVING,
   FLOORED
-}
+};
 
 // When the Code is Innitiated, the Float should be surfaced
 volatile Float_State float_curr_state = SURFACED;
@@ -97,7 +81,7 @@ volatile Float_State float_curr_state = SURFACED;
 enum Switch_State{
   ACTIVE,
   INACTIVE
-}
+};
 
 // Neither Switch Should be Activated when the code starts
 volatile Switch_State switch_top_state = INACTIVE;
@@ -111,13 +95,13 @@ enum Motor_Direction {
   CLOCKWISE,
   COUNTERCLOCKWISE,
   STALLED
-}
-
+};
 #pragma endregion
-
 
 #pragma region Setup
 void setup() {
+  #pragma region Pin_Definition
+
   analogWrite(pwm_port, DUTY_CYCLE);
   pinMode(RFM95_RST, OUTPUT);
   digitalWrite(RFM95_RST, HIGH);
@@ -127,9 +111,6 @@ void setup() {
   switch_bottom.setDebounceTime(0); 
 
   // Initiates how each pin on the board should work
-
-  // Try to set as addresses and pointers for the pins, to make it clearer what they do and which pins they are
-  #pragma region Pin_Definition
   pinMode(outA, OUTPUT);
   pinMode(outB, OUTPUT);
   pinMode(diag_port_A, INPUT_PULLUP);
@@ -164,7 +145,6 @@ void setup() {
   // set transfer power to 0
   rf95.setTxPower(23, false);
   // Attach interrupt handler for top switch
-  // May Remove?
   attachInterrupt(digitalPinToInterrupt(12), switchTopDetect, CHANGE);
   attachInterrupt(digitalPinToInterrupt(A3), switchBottomDetect, CHANGE);
 }
@@ -173,13 +153,7 @@ void setup() {
 //to init and define it 
 int16_t packetnum = 0;
 
-// multithreading / timer code setup
-// reminder* are all in terms of millis, so 1000 millis = 1 sec
-// Type - unsigned long, const long
-// All "const long" are basically the interval
-// for any newbies, const means it will never change, and long is simmilar to the double time, except it doesnt do decimal
-// unsigned is essentially a value type with only stores in positive integers for memory saving, which works here cuz we only count up
-
+// "Multithreading Section"
 unsigned long radio_task_millis = 0;
 unsigned long psi_task_half_millis = 0;
 unsigned long psi_task_full_millis = 0;
@@ -199,12 +173,10 @@ void loop() {
   // Millis Timer Start
   unsigned long current_millis = millis();
 
-  /// pressure calc
-  // makes sure that the pressure pin is set to A1
+  /// Pressure CaLCULATION
+    // makes sure that the pressure pin is set to A1
   int pressure_pin = analogRead(A1);
   float psi = (0.0374 * pressure_pin) - 3.3308;
-
-  // switch loop function, refer to function for more info 
 
 #pragma region Radio_Communications 
   
@@ -217,11 +189,8 @@ void loop() {
       uint8_t len = sizeof(buf);
 
       if (rf95.recv(buf, &len)) {
-        
-        
         // Store the received data in the global variable
         strncpy(received_data, (char*)buf, len);
-        
       } 
       else {
         //Receive failed
@@ -232,34 +201,16 @@ void loop() {
     }
   }
   // End of Radio Communication Checker
-
 #pragma endregion
-
 
   // Code which actualy starts/functions after passing the radio communcation check
   if (strcmp(received_data, "initiate") == 0) {
-    // Motor Movement Determiner Section
     // Reminder Top Switch is 12, and Bottom Switch is A3
-    // Motor Direction Reminder -
-      // outA - High, outB - Low ; Clockwise? the direction where it sucks water  
-      // outA - Low, outB - High ; Counter Clockwise? the direction where it pushes water out
-      /// NOTE, When either digital reads as "1", that means that the switch has yet to be hit
 
+    // Motor Movement Determiner
     float_curr_state = psiCompare(psi_half_sec, psi_full_sec, switch_bottom_state, switch_top_state);
     motorDirection(float_curr_state);
-    /*
-    else if (//Put code which determines if the float is at a depth of 2.5m){
-      digitalWrite(outA, LOW);                                                    
-      digitalWrite(outB, LOW);
-      digitalWrite(LED_BUILTIN, LOW);
-      if(//put timer code which will triger motor functions after 45s){
 
-      }
-    }
-    */
-
-    //// End of Motor Movement Determiner Section
-    //// Multi-threading part ٩( ᐖ )۶
 #pragma region PSI Data Code
     if (current_millis - list_updater_millis >= LIST_UPDATER_INTERVAL){
       list_updater_millis = current_millis;
@@ -277,19 +228,19 @@ void loop() {
     }
     /// Detrminer to decide whether or not the float is floored or surfaced
     /// Based on if there is a significant change in pressure
-    if (current_millis - psi_change_check_millis >= PSI_CHANGE_CHECK_INTERVAL)
+    if (current_millis - psi_change_check_millis >= PSI_CHANGE_CHECK_INTERVAL){
       psi_change_check_millis = current_millis;
 
       float_curr_state = psiCompare(psi_half_sec, psi_full_sec, switch_bottom_state, switch_top_state);
     }
   }
-#pragma endregion
-
 }
 #pragma endregion
 
+#pragma endregion
 
-// Functions (｡· v ·｡)
+
+// Functions
 #pragma region Functions
 
 // Calculates the change in PSI. If it is < 1, then it detects there is MINIMAL change in PSI, meaning that the Float is in one of 3 states
@@ -297,33 +248,47 @@ void loop() {
   // FLOORED - If the Bottom Switch is NOT PRESSED & the TOP Switch is PRESSED, then it must be FLOORED
   // SUBMURSED - If the Bottom Switch is NOT PRESSED & the NOT TOP Switch is PRESSED, then it must be SUBMURSED
 enum Float_State psiCompare(int half_time_psi, int full_time_psi, enum Switch_State switch_bottom_state, enum Switch_State switch_top_state){
-  int calc_psi_diff = abs(full_time_psi - half_time_psi);
-  if (calc_psi_diff <= 1){
+  float calc_psi_diff = abs(full_time_psi - half_time_psi);
+
+  if (calc_psi_diff <= 1.0){
     if(switch_bottom_state == ACTIVE && switch_top_state == INACTIVE){
       return SURFACED;
     } else if(switch_bottom_state == INACTIVE && switch_top_state == ACTIVE){
       return FLOORED;
     } else if(switch_bottom_state == INACTIVE && switch_top == INACTIVE){
       return SUBMURSED;
-    }
-  } else{
+    }  
+    } else{
     return MOVING;
   }
 }
 
 // Main Bulk of the Code
 void motorDirection(enum State float_state){
-  if (float_state == SURFACED){
-      // Makes the Motor Move Counter-Clockwise, So Suck In Water Making Float Move Down
-      digitalWrite(outA, LOW);                     
-      digitalWrite(outB, HIGH);
-      digitalWrite(LED_BUILTIN, LOW);
-  } else if(float_state == SUBMURSED || float_state == FLOORED){
-      // Makes the Motor Move Clockwise, So Push Out Water Making Float Move Up
-      digitalWrite(outA, HIGH);                     
-      digitalWrite(outB, LOW);
-      digitalWrite(LED_BUILTIN, LOW);
+  switch (float_state)
+  {
+  case SURFACED // Counter-Clockwise Motor Movemenet, Sucks in water
+    digitalWrite(outA, LOW);
+    digitalWrite(outB, HIGH);
+    break;
+  
+  case SUBMURSED // Clockwise Motor Movemenet, Pushes out water
+  // Add code where it'll stay at a certain depth for a certain amount of time before resuming movement
+    digitalWrite(outA, HIGH);
+    digitalWrite(outB, LOW);
+    break;
+  
+   case FLOORED // Clockwise Motor Movemenet, Pushes out water
+    digitalWrite(outA, HIGH);
+    digitalWrite(outB, LOW);
+    break;
+  
+  default: // defaults to have the motor to be stalled
+    digitalWrite(outA, LOW);
+    digitalWrite(outB, LOW);
+    break;
   }
+  digitalWrite(LED_BUILTIN, LOW); // Ensures switch will always have a resistor setup regardless of state
 }
 
 void switchBottomDetect(){
